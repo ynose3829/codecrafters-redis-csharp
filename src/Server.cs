51041a8f3a5348f4858f3c110da43786b321d435
Ynose <dls.y.nose@gmail.com>
2023-01-06 00:42:39 +0900

    2nd

--- src/Server.cs
+++ src/Server.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using System.Net.Sockets;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +12,8 @@ Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any,6379);
 server.Start();
-server.AcceptSocket();
+Socket socket = server.AcceptSocket();
 
 string respondingString = "PONG";
 Byte[] sendBytes = Encoding.ASCII.GetBytes(respondingString);
-int tmp = server.Send(sendBytes);
+int tmp = socket.Send(sendBytes);
