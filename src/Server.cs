6feb8f83d71a97d8b8dc2a9a8977e67168d51140
Ynose <dls.y.nose@gmail.com>
2023-01-06 00:21:51 +0900

    pass 1st stage

--- src/Server.cs
+++ src/Server.cs
@@ -8,3 +8,7 @@ Console.WriteLine("Logs from your program will appear here!");
 // TcpListener server = new TcpListener(IPAddress.Any, 6379);
 // server.Start();
 // server.AcceptSocket(); // wait for client
+
+TcpListener server = new TcpListener(IPAddress.Any,6379);
+server.Start();
+server.AcceptSocket();
